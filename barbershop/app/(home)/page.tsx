612ff39e9f3c6e704b4729a6ab89197e1c5be777
Olivio neto <netoolivio34@gmail.com>
2024-07-03 10:00:15 -0300

    feat: adicionando os itens iniciais da barbearia

--- barbershop/app/(home)/page.tsx
+++ barbershop/app/(home)/page.tsx
@@ -3,31 +3,52 @@ import Header from "../_components/header";
 import { ptBR } from "date-fns/locale";
 import Search from "./_components/search";
 import BookingItem from "../_components/booking-item";
+import { db } from "../_lib/prisma";
+import BarbershopItem from "./_components/barbershop-item";
 
-export default function Home() {
+export default async function Home() {
+
+  const barbershops = await db.barbershop.findMany({});
+
   return (
     <div>
-      <Header/>
+      <Header />
 
       <div className="px-5 pt-5">
-        <h2 className="text-xl font-bold">Olá, Olívio!</h2>
-          <p className="capitalize text-sm">
-            {format(new Date(), "EEEE',' dd 'de' MMMM",{
-              locale: ptBR
-            })}
-          </p>
-        </div>
+        <h2 className="text-xl font-bold">Olá, Guilherme!</h2>
+        <p className="capitalize text-sm">
+          {format(new Date(), "EEEE',' dd 'de' MMMM", {
+            locale: ptBR,
+          })}
+        </p>
+      </div>
 
-        <div className="px-5 mt-6">
-          <Search/>
-        </div>
+      <div className="px-5 mt-6">
+        <Search />
+      </div>
 
-        <div className="px-5 mt-5">
-          <h2 className="text-xs mb-3 uppercase text-gray-400 font-bold">
-            Agendamentos
-          </h2>
-            <BookingItem/>
-        </div>
+      <div className="px-5 mt-6">
+        <h2 className="text-xs uppercase text-gray-400 font-bold mb-3">Agendamentos</h2>
+        <BookingItem />
+      </div>
+
+      <div className="mt-6">
+        <h2 className="px-5 text-xs uppercase text-gray-400 font-bold mb-3">Recomendados</h2>
+          <div className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+            {barbershops.map((barbershop) => (
+              <BarbershopItem key={barbershop.id} barbershop={barbershop}/>
+            ))}
+          </div>
+      </div>
+      
+      {/* <div className="mt-6 mb-[4.5rem]">
+        <h2 className="px-5 text-xs uppercase text-gray-400 font-bold mb-3">Populares:</h2>
+          <div className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+            {barbershops.map((barbershop) => (
+              <BarbershopItem key={barbershop.id} barbershop={barbershop}/>
+            ))}
+          </div>
+      </div> */}
     </div>
   );
 }
--- barbershop/next.config.mjs
+++ barbershop/next.config.mjs
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // images:{
-    //     remotePatterns:{
-            
-    //     }
-    // }
+    images:{
+        remotePatterns:[
+            {
+                hostname: "utfs.io",
+            },
+        ],
+    },
 };
 
 export default nextConfig;
